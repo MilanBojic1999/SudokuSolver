1292cdbd1f31a0476b82cd99f51c8f5bb5f8c9bb
MilanBojic1999 <44372377+MilanBojic1999@users.noreply.github.com>
2019-09-25 16:30:21 +0200

    Main classes

---
+++ App.java
@@ -0,0 +1,13 @@
+package Sudoku;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+
+
+public class App extends Stage {
+
+    private GraphicsContext gc;
+
+
+}
---
+++ Spot.java
@@ -0,0 +1,73 @@
+package Sudoku;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Spot {
+    private int w,h;
+    private int num;
+    private GraphicsContext gc;
+    private int i,j;
+    private boolean selected;
+
+    public Spot(GraphicsContext gc,int i,int j) {
+        this.gc = gc;
+        this.i=i;
+        this.j=j;
+        w=h=100;
+        selected=false;
+        gc.setFont(Font.font(50));
+    }
+
+    public void show(){
+        if(selected){
+            gc.setFill(Color.SILVER);
+        }else{
+            gc.setFill(Color.SKYBLUE);
+        }
+        gc.setLineWidth(2);
+        gc.strokeRect(i*100,j*100,h,w);
+        gc.fillRect(i*100,j*100,h,w);
+        gc.setFill(Color.BLACK);
+        if(num!=0){
+            gc.fillText(Integer.toString(num),i*100+40,j*100+60);
+        }
+    }
+
+    public void showMistake(){
+        gc.setLineWidth(7);
+        gc.setFill(Color.INDIANRED);
+        gc.strokeRect(i*100,j*100,h,w);
+        gc.fillRect(i*100,j*100,h,w);
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    @Override
+    public String toString() {
+        return i+" "+j;
+    }
+
+    public boolean isEmpty(){
+        return num==0;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+}
